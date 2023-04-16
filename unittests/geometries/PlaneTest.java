5478df3a33204730363e388d3922a0e080fb375a
רעיה פלדמר <raaya.marmor@gmail.com>
2023-04-16 13:12:56 +0300

    CHANGE OF YAIR

--- unittests/geometries/PlaneTest.java
+++ unittests/geometries/PlaneTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import primitives.Point;
 import primitives.Vector;
 
-import static java.lang.System.out;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -17,14 +16,14 @@ class PlaneTest {
     Point po3=new Point(0,0,0);
     Vector v1=new Vector(0,0,1);
     Vector v2=new Vector(0,0,-1);
-    Plane pl1= new Plane(po1,po2,po3);
-    Plane pl2=new Plane(po1,v1);
+    Plane plane = new Plane(po1,po2,po3);
+    Plane plane2 =new Plane(po1,v1);
 
     @Test
     public void testConstructor() {
         // ============ Equivalence Partitions Tests ==============
         // TC01: Test that the constructor proper
-        assertEquals(pl2,pl1,"ERROR: constructor() the plane is incorrect");
+        assertEquals(plane2, plane,"ERROR: constructor() the plane is incorrect");
         // =============== Boundary Values Tests ==================
         //TC10: Test that throws exception if the first point equals to the third point.
         assertThrows(IllegalArgumentException.class, ()->new Plane(po1,po2,new Point(1,0,0)),"ERROR: constructor() the first point equals to the third point");
@@ -34,7 +33,7 @@ class PlaneTest {
     }
     @Test
     public void testGetNormal() {
-        Vector normal = pl1.getNormal(po3);
+        Vector normal = plane.getNormal(po3);
         // ============ Equivalence Partitions Tests ==============
         // TC01: Test that the normal's length equals to 1.
         assertEquals(1d, normal.length(),0.00001, "ERROR: getNormal() the normal is not normalized");
--- unittests/geometries/SphereTest.java
+++ unittests/geometries/SphereTest.java
@@ -8,20 +8,21 @@ import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Unit tests for geometries.Sphere class
+ *
  * @author raaya feldmar
  */
 class SphereTest {
 
-    Sphere s1= new Sphere(1,new Point(0,0,0));
+    Sphere s1 = new Sphere(1, new Point(0, 0, 0));
 
     @Test
     public void testGetNormal() {
-        Vector normal= s1.getNormal(new Point(0,0,1));
-        Vector v1= new Vector(0,0,1);
+        Vector normal = s1.getNormal(new Point(0, 0, 1));
+        Vector v1 = new Vector(0, 0, 1);
         // ============ Equivalence Partitions Tests ==============
         // TC01: Test that the normal's length equals to 1.
-        assertEquals(1d,normal.length(),0.00001d,"ERROR: getNormal() the normal is not normalized");
+        assertEquals(1d, normal.length(), 0.00001d, "ERROR: getNormal() the normal is not normalized");
         //TC02: Test that the normal is orthogonal to the sphere
-        assertEquals(v1,normal,"ERROR: getNormal() the normal does not orthogonal to the sphere");
+        assertEquals(v1, normal, "ERROR: getNormal() the normal does not orthogonal to the sphere");
     }
 }
